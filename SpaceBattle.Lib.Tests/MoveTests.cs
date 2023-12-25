2da972130695d6157d1cc560044118379d06465f
Nikitpolin <polinann080@gmail.com>
2023-12-25 17:34:31 +0000

    вроде все работает

---
+++ SpaceBattle.Lib.Tests/MoveTests.cs
@@ -0,0 +1,60 @@
+﻿namespace SpaceBattle.Lib.Tests;
+
+public class MovableTests
+{
+    [Fact]
+    public void MoveCommand_StraightLineMovementComplete()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Vector(new int[] { 12, 5 })).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new Vector(new int[] { -7, 3 })).Verifiable();
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        moveCommand.Execute();
+
+        movable.VerifySet(m => m.Position = new Vector(new int[] { 5, 8 }), Times.Once);
+        movable.VerifyAll();
+    }
+
+    [Fact]
+    public void MoveCommand_CantReadPosition()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Throws(new NotImplementedException());
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        Assert.Throws<NotImplementedException>(moveCommand.Execute);
+    }
+
+    [Fact]
+    public void MoveCommand_CantReadVelocity()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Vector(new int[] { 1, 4 })).Verifiable();
+        movable.SetupGet(m => m.Velocity).Throws(new NotImplementedException());
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        Assert.Throws<NotImplementedException>(moveCommand.Execute);
+    }
+
+    [Fact]
+    public void MoveCommand_CantSetPosition()
+    {
+        var movable = new Mock<IMovable>();
+
+        movable.SetupGet(m => m.Position).Returns(new Vector(new int[] { 1, 4 })).Verifiable();
+        movable.SetupGet(m => m.Velocity).Returns(new Vector(new int[] { 1, 2 })).Verifiable();
+        movable.SetupSet(m => m.Position = It.IsAny<Vector>()).Throws(new NotImplementedException());
+
+        ICommand moveCommand = new MoveCommand(movable.Object);
+
+        Assert.Throws<NotImplementedException>(moveCommand.Execute);
+        movable.VerifyAll();
+    }
+}
---
+++ SpaceBattle.Lib.Tests/Rational_Tests.cs
@@ -0,0 +1,82 @@
+﻿namespace SpaceBattle.Lib.Tests
+{
+    public class Rational_Tests
+    {
+        [Fact]
+        public void Rational_Equals()
+        {
+            var angle = new Rational(45);
+            var delta = new Rational(45);
+            Assert.True(angle == delta);
+        }
+
+        [Fact]
+        public void RationalUnequals()
+        {
+            var angle = new Rational(45, -360);
+            var delta = new Rational(90);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalUnequalsNumerator()
+        {
+            var angle = new Rational(13, 360);
+            var delta = new Rational(17);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalUnequalsNumeratorAndDenomirator()
+        {
+            var angle = new Rational(1, 2);
+            var delta = new Rational(2, 3);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalUnequalsDenomirator()
+        {
+            var angle = new Rational(1, 2);
+            var delta = new Rational(1, 6);
+            Assert.True(angle != delta);
+        }
+
+        [Fact]
+        public void RationalEqualsNumeratorAndDenomiratorWrong()
+        {
+            var angle = new Rational(1, 2);
+            var delta = new Rational(2, 3);
+            Assert.False(angle == delta);
+        }
+
+        [Fact]
+        public void RationalDenominatorIsZero()
+        {
+            Assert.Throws<Exception>(() => new Rational(17, 0));
+        }
+
+        [Fact]
+        public void Rational_HashCode()
+        {
+            var angle = new Rational(45);
+            var delta = new Rational(45);
+            Assert.Equal(angle.GetHashCode(), delta.GetHashCode());
+        }
+
+        [Fact]
+        public void Rational_Equals_Null()
+        {
+            var angle = new Rational(45);
+            Assert.False(angle.Equals(null));
+        }
+
+        [Fact]
+        public void RationalEqualsFalseNumerator()
+        {
+            var angle = new Rational(45);
+            var delta = new Rational(45, 360);
+            Assert.True(angle.Equals(delta));
+        }
+    }
+}
