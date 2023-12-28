74a4000777b7553106b2ad008cf1bc00653cbc6a
Nikitpolin <polinann080@gmail.com>
2023-12-28 08:46:59 +0000

    все исправила

--- SpaceBattle.Lib.Tests/Rational_Tests.cs
+++ SpaceBattle.Lib.Tests/Rational_Tests.cs
@@ -1,81 +1,87 @@
-﻿namespace SpaceBattle.Lib.Tests
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SpaceBattle.Lib.Tests
 {
     public class Rational_Tests
     {
         [Fact]
         public void Rational_Equals()
         {
-            var angle = new Rational(45);
-            var delta = new Rational(45);
+            Rational angle = new Rational(45);
+            Rational delta = new Rational(45);
             Assert.True(angle == delta);
         }
 
         [Fact]
         public void RationalUnequals()
         {
-            var angle = new Rational(45, -360);
-            var delta = new Rational(90);
+            Rational angle = new Rational(45, -360);
+            Rational delta = new Rational(90);
             Assert.True(angle != delta);
         }
 
         [Fact]
         public void RationalUnequalsNumerator()
         {
-            var angle = new Rational(13, 360);
-            var delta = new Rational(17);
+            Rational angle = new Rational(13,360);
+            Rational delta = new Rational(17);
             Assert.True(angle != delta);
         }
 
         [Fact]
         public void RationalUnequalsNumeratorAndDenomirator()
         {
-            var angle = new Rational(1, 2);
-            var delta = new Rational(2, 3);
+            Rational angle = new Rational(1,2);
+            Rational delta = new Rational(2,3);
             Assert.True(angle != delta);
         }
 
         [Fact]
         public void RationalUnequalsDenomirator()
         {
-            var angle = new Rational(1, 2);
-            var delta = new Rational(1, 6);
+            Rational angle = new Rational(1,2);
+            Rational delta = new Rational(1,6);
             Assert.True(angle != delta);
         }
 
         [Fact]
         public void RationalEqualsNumeratorAndDenomiratorWrong()
         {
-            var angle = new Rational(1, 2);
-            var delta = new Rational(2, 3);
+            Rational angle = new Rational(1,2);
+            Rational delta = new Rational(2,3);
             Assert.False(angle == delta);
         }
 
         [Fact]
         public void RationalDenominatorIsZero()
         {
-            Assert.Throws<Exception>(() => new Rational(17, 0));
+           Assert.Throws<Exception>(() => new Rational(17,0));
         }
 
         [Fact]
         public void Rational_HashCode()
         {
-            var angle = new Rational(45);
-            var delta = new Rational(45);
+            Rational angle = new Rational(45);
+            Rational delta = new Rational(45);
             Assert.Equal(angle.GetHashCode(), delta.GetHashCode());
         }
 
         [Fact]
         public void Rational_Equals_Null()
         {
-            var angle = new Rational(45);
+            Rational angle = new Rational(45);
             Assert.False(angle.Equals(null));
         }
 
         [Fact]
         public void RationalEqualsFalseNumerator()
         {
-            var angle = new Rational(45);
-            var delta = new Rational(45, 360);
+            Rational angle = new Rational(45);
+            Rational delta = new Rational(45,360);
             Assert.True(angle.Equals(delta));
         }
     }
--- SpaceBattle.Lib.Tests/Turn_Command.cs
+++ SpaceBattle.Lib.Tests/Turn_Command.cs
@@ -5,11 +5,11 @@ public class TurnCommand_Tests
     [Fact]
     public void TurnCommandPositive()
     {
-        var turnable = new Mock<ITurnable>();
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
         turnable.SetupGet(m => m.angle).Returns(new Rational(45)).Verifiable();
         turnable.SetupGet(m => m.delta).Returns(new Rational(45)).Verifiable();
 
-        var turnCommand = new TurnCommand(turnable.Object);
+        TurnCommand turnCommand = new TurnCommand(turnable.Object);
         turnCommand.Execute();
 
         turnable.VerifySet(m => m.angle = new Rational(90), Times.Once);
@@ -18,33 +18,33 @@ public class TurnCommand_Tests
     [Fact]
     public void CantDetermineAngle()
     {
-        var turnable = new Mock<ITurnable>();
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
         turnable.SetupGet(m => m.angle).Throws(new Exception()).Verifiable();
         turnable.SetupGet(m => m.delta).Returns(new Rational(45)).Verifiable();
 
-        var turnCommand = new TurnCommand(turnable.Object);
+        TurnCommand turnCommand = new TurnCommand(turnable.Object);
         Assert.Throws<Exception>(() => turnCommand.Execute());
     }
 
     [Fact]
     public void CantDetermineDelta()
     {
-        var turnable = new Mock<ITurnable>();
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
         turnable.SetupGet(m => m.angle).Returns(new Rational(45)).Verifiable();
         turnable.SetupGet(m => m.delta).Throws(new Exception()).Verifiable();
 
-        var turnCommand = new TurnCommand(turnable.Object);
+        TurnCommand turnCommand = new TurnCommand(turnable.Object);
         Assert.Throws<Exception>(() => turnCommand.Execute());
     }
 
     [Fact]
     public void CantChangeAngle()
     {
-        var turnable = new Mock<ITurnable>();
+        Mock<ITurnable> turnable = new Mock<ITurnable>();
         turnable.SetupGet(m => m.angle).Returns(new Rational(45)).Verifiable();
         turnable.SetupGet(m => m.delta).Returns(new Rational(45)).Verifiable();
         turnable.SetupSet(m => m.angle = It.IsAny<Rational>()).Throws(new Exception()).Verifiable();
-        var turnCommand = new TurnCommand(turnable.Object);
+        TurnCommand turnCommand = new TurnCommand(turnable.Object);
 
         Assert.Throws<Exception>(() => turnCommand.Execute());
     }
